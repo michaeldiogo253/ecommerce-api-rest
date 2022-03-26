1fd25ca3bf8c95b4974d0bb4d043f5ca0cd7f3d0
MichaelDiogo <michaeldiogo17@outlook.com>
2022-03-26 12:14:59 -0300

    feat: criacao Service para adicionar produto em um  carrinho

--- src/main/java/estudos/ecommerce/carrinho/application/service/AdicionaProdutoNoCarrinhoService.java
+++ src/main/java/estudos/ecommerce/carrinho/application/service/AdicionaProdutoNoCarrinhoService.java
@@ -1,9 +1,11 @@
 package estudos.ecommerce.carrinho.application.service;
 
-import estudos.ecommerce.carrinho.application.port.in.AdicionarProdutoNoCarrinhoUseCase;
+import estudos.ecommerce.carrinho.application.port.in.AdicionaProdutoEmUmNovoCarrinhoUseCase;
+import estudos.ecommerce.carrinho.application.port.in.AdicionaProdutoNoCarrinhoUseCase;
 import estudos.ecommerce.carrinho.application.port.out.FindCarrinhoByIdClientePort;
 import estudos.ecommerce.carrinho.domain.Carrinho;
-import estudos.ecommerce.carrinho.domain.ItemDoCarrinho;
+import estudos.ecommerce.itemdocarrinho.application.port.in.BuscaItemDoCarrinhoUseCase;
+import estudos.ecommerce.itemdocarrinho.domain.ItemDoCarrinho;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,22 +16,26 @@ import java.util.Optional;
 @Transactional
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class AdicionaProdutoNoCarrinhoService implements AdicionarProdutoNoCarrinhoUseCase {
+public class AdicionaProdutoNoCarrinhoService implements AdicionaProdutoNoCarrinhoUseCase {
 
     private final FindCarrinhoByIdClientePort findCarrinhoByIdClientePort;
+    private final BuscaItemDoCarrinhoUseCase buscaItemDoCarrinhoUseCase;
+    private final AdicionaProdutoEmUmCarrinhoExistenteService adicionaProdutoEmUmCarrinhoExistenteService;
+    private final AdicionaProdutoEmUmNovoCarrinhoUseCase adicionaProdutoEmUmNovoCarrinhoUseCase;
 
     @Override
-    public void execute(Long idCliente, Long idProduto, Integer quantidade) {
+    public Carrinho execute(Long idCliente, Long idProduto, Integer quantidade) {
+
         Optional<Carrinho> carrinhoBuscado = findCarrinhoByIdClientePort.findCarrinhoByIdCliente(idCliente);
 
         if (carrinhoBuscado.isPresent()) {
-            ItemDoCarrinho itemDoCarrinho = itemDoCarrinhoService.buscaItemCarrinho(carrinhoBuscado.get(),
-                                                                                    idProduto,
-                                                                                    quantidade);
 
-            return adicionaProdutoEmCarrinhoExistente(carrinhoBuscado.get(), itemDoCarrinho, request.getQuantidade());
+
+            ItemDoCarrinho itemDoCarrinho = buscaItemDoCarrinhoUseCase.execute(carrinhoBuscado.get(), idProduto, quantidade);
+
+            return adicionaProdutoEmUmCarrinhoExistenteService.adicionaProdutoEmCarrinhoExistente(carrinhoBuscado.get(), itemDoCarrinho, quantidade);
         }
 
-        return criaNovoCarrinhoEAdicionaProduto(request);
+        return adicionaProdutoEmUmNovoCarrinhoUseCase.execute(idCliente, idProduto, quantidade);
     }
 }
