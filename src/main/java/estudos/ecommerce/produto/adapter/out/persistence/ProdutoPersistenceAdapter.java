60060e0bcca3b36b5b7bc7eb34eb126de076974e
MichaelDiogo <michaeldiogo17@outlook.com>
2022-03-26 14:41:35 -0300

    feat: criacao adaptador de persistencia do Produto

--- src/main/java/estudos/ecommerce/produto/adapter/out/persistence/ProdutoPersistenceAdapter.java
+++ src/main/java/estudos/ecommerce/produto/adapter/out/persistence/ProdutoPersistenceAdapter.java
@@ -1,6 +1,8 @@
 package estudos.ecommerce.produto.adapter.out.persistence;
 
 import estudos.ecommerce.produto.application.port.out.DeleteProdutoByIdPort;
+import estudos.ecommerce.produto.application.port.out.FindAllProdutoPort;
+import estudos.ecommerce.produto.application.port.out.FindProdutoByIdPort;
 import estudos.ecommerce.produto.application.port.out.SaveProdutoPort;
 import estudos.ecommerce.produto.domain.Produto;
 import estudos.ecommerce.util.exception.ResourceNotFoundException;
@@ -8,10 +10,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class ProdutoPersistenceAdapter implements SaveProdutoPort, DeleteProdutoByIdPort {
+public class ProdutoPersistenceAdapter implements SaveProdutoPort,
+                                                  DeleteProdutoByIdPort,
+                                                  FindProdutoByIdPort ,
+                                                  FindAllProdutoPort {
 
     private final ProdutoRepository produtoRepository;
 
@@ -29,5 +35,17 @@ public class ProdutoPersistenceAdapter implements SaveProdutoPort, DeleteProduto
             return;
         }
         throw new ResourceNotFoundException("Produto não encontrado");
+    }
+
+    @Override
+    public Produto findProdutoById(Long idProduto) {
+
+        return produtoRepository.findById(idProduto).orElseThrow(()-> new ResourceNotFoundException("Produto não encontrado"));
+    }
+
+    @Override
+    public List<Produto> listarTodosOsProdutos() {
+
+        return produtoRepository.findAll();
     }
 }
