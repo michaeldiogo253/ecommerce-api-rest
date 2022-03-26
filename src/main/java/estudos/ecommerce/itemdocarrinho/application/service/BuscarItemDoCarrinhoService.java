83f393817a977eb863ec52f9c8b5a7dac19ea012
MichaelDiogo <michaeldiogo17@outlook.com>
2022-03-26 14:40:06 -0300

    feat: criacao Service e UseCase para buscar produto no carrinho

---
+++ src/main/java/estudos/ecommerce/itemdocarrinho/application/service/BuscarItemDoCarrinhoService.java
@@ -0,0 +1,38 @@
+package estudos.ecommerce.itemdocarrinho.application.service;
+
+import estudos.ecommerce.carrinho.domain.Carrinho;
+import estudos.ecommerce.itemdocarrinho.application.port.in.BuscaItemDoCarrinhoUseCase;
+import estudos.ecommerce.itemdocarrinho.application.port.in.CriaNovoItemCarrinhoUseCase;
+import estudos.ecommerce.itemdocarrinho.application.port.out.FindItemCarrinhoByIdItemAndProdutoIdPort;
+import estudos.ecommerce.itemdocarrinho.domain.ItemDoCarrinho;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class BuscarItemDoCarrinhoService implements BuscaItemDoCarrinhoUseCase {
+
+    private final FindItemCarrinhoByIdItemAndProdutoIdPort findItemCarrinhoByIdItemAndProdutoIdPort;
+    private final CriaNovoItemCarrinhoUseCase criaNovoItemCarrinhoUseCase;
+
+    @Override
+    public ItemDoCarrinho execute(Carrinho carrinho, Long produtoId, Integer quantidade) {
+
+        Optional<ItemDoCarrinho> itemCarrinho =
+                findItemCarrinhoByIdItemAndProdutoIdPort.findItemCarrinhoByIdItemAndProdutoIdPort(
+                        carrinho.getId(),
+                        produtoId);
+
+        if(itemCarrinho.isPresent()){
+            return itemCarrinho.get();
+        }
+
+        return criaNovoItemCarrinhoUseCase.criaNovoItemDoCarrinho(produtoId, quantidade, carrinho);
+
+    }
+}
