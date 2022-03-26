575cf8bb442c19238eb8685e8dec9b3772f9b2b4
MichaelDiogo <michaeldiogo17@outlook.com>
2022-03-26 14:45:24 -0300

    feta: criação de de Service e UseCase para cria um novo Item de carinho

--- src/main/java/estudos/ecommerce/carrinho/adapter/out/persistence/CarrinhoPersistenceAdapter.java
+++ src/main/java/estudos/ecommerce/carrinho/adapter/out/persistence/CarrinhoPersistenceAdapter.java
@@ -1,6 +1,7 @@
 package estudos.ecommerce.carrinho.adapter.out.persistence;
 
 import estudos.ecommerce.carrinho.application.port.out.FindCarrinhoByIdClientePort;
+import estudos.ecommerce.carrinho.application.port.out.SaveCarrinhoPort;
 import estudos.ecommerce.carrinho.domain.Carrinho;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class CarrinhoPersistenceAdapter implements FindCarrinhoByIdClientePort {
+public class CarrinhoPersistenceAdapter implements FindCarrinhoByIdClientePort, SaveCarrinhoPort {
 
     private final CarrinhoRepository carrinhoRepository;
 
@@ -18,5 +19,11 @@ public class CarrinhoPersistenceAdapter implements FindCarrinhoByIdClientePort {
     public Optional<Carrinho> findCarrinhoByIdCliente(Long idCliente) {
 
         return carrinhoRepository.findByIdCliente(idCliente);
+    }
+
+    @Override
+    public Carrinho salvarCarrinho(Carrinho carrinho) {
+
+        return carrinhoRepository.save(carrinho);
     }
 }
