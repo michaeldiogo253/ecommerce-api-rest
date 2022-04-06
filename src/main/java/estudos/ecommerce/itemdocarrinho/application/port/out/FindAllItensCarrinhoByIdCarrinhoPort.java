2f1fd0150934ae348c34cf5dd2f2177b4058678a
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-05 22:24:29 -0300

    feat: criação de port FindAllItensCarrinhoByIdCarrinhoPort

---
+++ src/main/java/estudos/ecommerce/itemdocarrinho/application/port/out/FindAllItensCarrinhoByIdCarrinhoPort.java
@@ -0,0 +1,10 @@
+package estudos.ecommerce.itemdocarrinho.application.port.out;
+
+import estudos.ecommerce.itemdocarrinho.domain.ItemDoCarrinho;
+
+import java.util.List;
+
+public interface FindAllItensCarrinhoByIdCarrinhoPort {
+
+    List<ItemDoCarrinho> findItensDoCarrinhoByIdCarrinho(Long idCarrinho);
+}
