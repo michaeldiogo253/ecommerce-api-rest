4c39ddd200aec3d06e86e1dec4aedc2af6b3dd20
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-12 23:18:16 -0300

    refactor! formatação para melhorar legibilidade do código

--- src/main/java/estudos/ecommerce/cliente/application/service/ListarTodosClientesService.java
+++ src/main/java/estudos/ecommerce/cliente/application/service/ListarTodosClientesService.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ListarTodosClientesService implements ListarTodosOsClientesUseCase {
 
     private final FindAllClientesPort findAllClientesPort;
+
     @Override
     public List<Cliente> execute() {
 
