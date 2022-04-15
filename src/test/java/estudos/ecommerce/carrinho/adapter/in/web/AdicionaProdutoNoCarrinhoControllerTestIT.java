90c84919e281ce2d89ee86b0c260a72cc75c21de
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-14 21:28:22 -0300

    test: criação de teste de integração

---
+++ src/test/java/estudos/ecommerce/carrinho/adapter/in/web/AdicionaProdutoNoCarrinhoControllerTestIT.java
@@ -0,0 +1,23 @@
+package estudos.ecommerce.carrinho.adapter.in.web;
+
+import estudos.ecommerce.carrinho.application.port.in.AdicionaProdutoNoCarrinhoUseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AdicionaProdutoNoCarrinhoControllerTestIT {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private AdicionaProdutoNoCarrinhoUseCase adicionarProdutoNoCarrinhoUseCase;
+
+    @Test
+    void deveriaAdicionaProdutoNoCarrinhoEDevolverStatusCode201(){
+
+    }
+}
