0fff2c48367f80d6c1e8151a55dcf8094c9a957f
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-17 20:12:39 -0300

    refactor! alterado chamada de metodo da request para modo estatico

--- src/main/java/estudos/ecommerce/produto/adapter/in/web/CadastrarProdutoController.java
+++ src/main/java/estudos/ecommerce/produto/adapter/in/web/CadastrarProdutoController.java
@@ -27,7 +27,7 @@ public class CadastrarProdutoController {
     public ResponseEntity<ProdutoResponse> cadastrarProduto(@RequestBody @Valid ProdutoRequest request,
                                                             UriComponentsBuilder uriBuilder) {
 
-        Produto produtoCriado = cadastrarProdutoUseCase.execute(request.toModel(request), request.getCategoria());
+        Produto produtoCriado = cadastrarProdutoUseCase.execute(ProdutoRequest.toModel(request), request.getCategoria());
         URI uri = uriBuilder.path("ecommerce-api/produto/{id}")
                             .buildAndExpand(produtoCriado.getId())
                             .toUri();
