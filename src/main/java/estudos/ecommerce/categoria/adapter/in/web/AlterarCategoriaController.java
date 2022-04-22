e0b633b0ab026488b5f51e640fcb7b4724aa5530
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-22 19:25:42 -0300

    feat: criação de controller para alterar categoria do produto.

--- src/main/java/estudos/ecommerce/categoria/adapter/in/web/AlterarCategoriaController.java
+++ src/main/java/estudos/ecommerce/categoria/adapter/in/web/AlterarCategoriaController.java
@@ -1,13 +1,16 @@
 package estudos.ecommerce.categoria.adapter.in.web;
 
 import estudos.ecommerce.categoria.adapter.in.web.request.AlterarCategoriaRequest;
+import estudos.ecommerce.categoria.adapter.in.web.response.CategoriaResponse;
 import estudos.ecommerce.categoria.application.port.in.AlterarCategoriaUseCase;
+import estudos.ecommerce.categoria.domain.Categoria;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 
@@ -17,12 +20,12 @@ public class AlterarCategoriaController {
 
     private final AlterarCategoriaUseCase alterarCategoriaUseCase;
 
-    @PutMapping("")
-    public ResponseEntity<Void> alterarCategoria(@RequestBody @Valid AlterarCategoriaRequest request){
+    @PutMapping("/ecommerce-api/categoria/atualizar")
+    public ResponseEntity<CategoriaResponse> alterarCategoria(@RequestBody @Valid AlterarCategoriaRequest request,
+                                                              UriComponentsBuilder uriBuilder){
 
-        alterarCategoriaUseCase.execute(request.getIdCategoria(), request.getNome());
+        Categoria categoriaAtualizada = alterarCategoriaUseCase.execute(request.getIdCategoria(), request.getNome());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(CategoriaResponse.from(categoriaAtualizada));
     }
-
 }
