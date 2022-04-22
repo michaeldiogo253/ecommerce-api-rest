ba3d32e2f054ded46166a3afd9ea2f5dbf943639
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-22 19:25:22 -0300

    feat: criação de use case para alterar categoria do produto.

--- src/main/java/estudos/ecommerce/categoria/application/port/in/AlterarCategoriaUseCase.java
+++ src/main/java/estudos/ecommerce/categoria/application/port/in/AlterarCategoriaUseCase.java
@@ -1,6 +1,8 @@
 package estudos.ecommerce.categoria.application.port.in;
 
+import estudos.ecommerce.categoria.domain.Categoria;
+
 public interface AlterarCategoriaUseCase {
 
-    void execute(Long idCategoria, String novoNomeCategoria);
+    Categoria execute(Long idCategoria, String novoNomeCategoria);
 }
