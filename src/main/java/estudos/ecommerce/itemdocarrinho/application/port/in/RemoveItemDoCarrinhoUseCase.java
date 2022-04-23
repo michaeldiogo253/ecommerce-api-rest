a2ac18d70e4eeec954783e1b8c67735f9c2d9044
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-23 13:12:56 -0300

    refactor! alterado nome de classe para deixa-la semanticamente correta conforme ação que classe realiza.

--- src/main/java/estudos/ecommerce/itemdocarrinho/application/port/in/RemoveItemDoCarrinhoUseCase.java
+++ src/main/java/estudos/ecommerce/itemdocarrinho/application/port/in/RemoveItemDoCarrinhoUseCase.java
@@ -1,9 +1,7 @@
 package estudos.ecommerce.itemdocarrinho.application.port.in;
 
-import estudos.ecommerce.carrinho.domain.Carrinho;
-import estudos.ecommerce.itemdocarrinho.domain.ItemDoCarrinho;
-
 public interface RemoveItemDoCarrinhoUseCase {
 
-    ItemDoCarrinho execute(Carrinho carrinho, Long idProduto, Integer quantidade);
+    void execute(Long idCliente, Long idProduto);
+
 }
