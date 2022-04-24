41bc34faa01efebdac959c52ee6f2697a30f4da8
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-24 14:27:31 -0300

    feat: implementação de logica do service para remover produto do carrinho

--- src/main/java/estudos/ecommerce/carrinho/application/service/RemoveItemDoCarrinhoService.java
+++ src/main/java/estudos/ecommerce/carrinho/application/service/RemoveItemDoCarrinhoService.java
@@ -1,6 +1,12 @@
 package estudos.ecommerce.carrinho.application.service;
 
+import estudos.ecommerce.carrinho.application.port.out.DeleteCarrinhoByIdPort;
+import estudos.ecommerce.carrinho.application.port.out.FindCarrinhoByIdClientePort;
+import estudos.ecommerce.carrinho.application.port.out.SaveCarrinhoPort;
+import estudos.ecommerce.carrinho.domain.Carrinho;
 import estudos.ecommerce.itemdocarrinho.application.port.in.RemoveItemDoCarrinhoUseCase;
+import estudos.ecommerce.itemdocarrinho.application.port.out.FindItemCarrinhoByIdCarrinhoAndIdProdutoPort;
+import estudos.ecommerce.itemdocarrinho.domain.ItemDoCarrinho;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +18,30 @@ import javax.transaction.Transactional;
 @Transactional
 public class RemoveItemDoCarrinhoService implements RemoveItemDoCarrinhoUseCase {
 
+    private final FindCarrinhoByIdClientePort findCarrinhoByIdClientePort;
+    private final FindItemCarrinhoByIdCarrinhoAndIdProdutoPort findItemCarrinhoByIdCarrinhoAndIdProdutoPort;
+    private final DeleteCarrinhoByIdPort deleteCarrinhoByIdPort;
+    private final SaveCarrinhoPort saveCarrinhoPort;
+
     @Override
     public void execute(Long idCliente, Long idProduto) {
-        //todo implementar logica de remover item do carrinho
+        Carrinho carrinho = findCarrinhoByIdClientePort.findCarrinhoByIdCliente(idCliente)
+                                                       .orElseThrow(() -> new IllegalArgumentException(
+                                                               "Carrinho do cliente não encontrado"));
+       var itemDoCarrinho =
+                findItemCarrinhoByIdCarrinhoAndIdProdutoPort.findItemCarrinhoByIdCarrinhoAndIdProduto(
+                carrinho.getId(),
+                idProduto).orElseThrow(() -> new IllegalArgumentException(
+                        "Item do Carrinho não encontrado"));
+
+        carrinho.removeItemDoCarrinho(itemDoCarrinho);
+        carrinho.atualizaTotaisDoCarrinho();
+
+        if(carrinho.carrinhoIsVazio()){
+            deleteCarrinhoByIdPort.deleteCarrinhoPorId(carrinho.getId());
+            return;
+        }
+
+        saveCarrinhoPort.salvarCarrinho(carrinho);
     }
 }
