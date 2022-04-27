2c6abef3308e1887bd21f2822c188f554848f21e
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-26 22:07:57 -0300

    feat: criação de response para listar carrinho

---
+++ src/main/java/estudos/ecommerce/carrinho/adapter/in/web/response/CarrinhoResponse.java
@@ -0,0 +1,28 @@
+package estudos.ecommerce.carrinho.adapter.in.web.response;
+
+import estudos.ecommerce.carrinho.domain.Carrinho;
+import estudos.ecommerce.itemdocarrinho.adapter.in.web.response.ItemDoCarrinhoResponse;
+import lombok.Value;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Value
+public class CarrinhoResponse {
+
+    Long idCarrinho;
+    String nomeCliente;
+    List<ItemDoCarrinhoResponse> itens;
+    BigDecimal total;
+    Integer quantidadeItens;
+
+    public static CarrinhoResponse from(Carrinho carrinho) {
+
+        return new CarrinhoResponse(carrinho.getId(),
+                                    carrinho.getNomeCliente(),
+                                    ItemDoCarrinhoResponse.of(carrinho.getCarrinho()),
+                                    carrinho.getTotal(),
+                                    carrinho.getQuantidadeDeItensDoCarrinho());
+    }
+
+}
