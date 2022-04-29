86e8ebbc92374ef0fddb50ddebd83f56b660f5c5
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-28 22:03:52 -0300

    refactor! adiação de propriedade unique no nome do cliente e no cpf

--- src/main/java/estudos/ecommerce/cliente/domain/DadosPessoais.java
+++ src/main/java/estudos/ecommerce/cliente/domain/DadosPessoais.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.UniqueConstraint;
 import java.time.LocalDate;
 
 @Getter
@@ -15,7 +17,9 @@ import java.time.LocalDate;
 @Embeddable
 public class DadosPessoais {
 
+    @Column(unique = true)
     private String nome;
+    @Column(unique = true)
     private String cpf;
     private String telefone;
     private LocalDate dataNasc;
