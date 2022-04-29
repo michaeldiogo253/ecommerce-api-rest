37cf80739440a8aac94eae6fafedd00e9e33b8be
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-28 22:07:20 -0300

    refactor! formatação de código

--- src/test/java/estudos/ecommerce/cliente/adapter/in/web/ListarTodosOsClientesControllerTestIT.java
+++ src/test/java/estudos/ecommerce/cliente/adapter/in/web/ListarTodosOsClientesControllerTestIT.java
@@ -1,17 +1,13 @@
 package estudos.ecommerce.cliente.adapter.in.web;
 
-import estudos.ecommerce.cliente.adapter.in.web.response.ClienteResponse;
-import estudos.ecommerce.cliente.application.port.in.ListarTodosOsClientesUseCase;
-import estudos.ecommerce.cliente.domain.Cliente;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ListarTodosOsClientesControllerTestIT {
