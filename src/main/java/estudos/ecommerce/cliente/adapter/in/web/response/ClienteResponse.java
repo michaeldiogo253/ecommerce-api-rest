ea9c62a450c99ffe9434894104b96bc0a5246b7e
MichaelDiogo <michaeldiogo17@outlook.com>
2022-05-21 13:43:10 -0300

    refactor! adição de anotação @NoArgsContructor

--- src/main/java/estudos/ecommerce/cliente/adapter/in/web/response/ClienteResponse.java
+++ src/main/java/estudos/ecommerce/cliente/adapter/in/web/response/ClienteResponse.java
@@ -3,6 +3,7 @@ package estudos.ecommerce.cliente.adapter.in.web.response;
 import estudos.ecommerce.cliente.domain.Cliente;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class ClienteResponse {
     private Long id;
     private String nome;
