1556a1baf81a86b72378db6fcec89ddc6f227e8a
MichaelDiogo <michael@petize.com.br>
2022-06-09 22:37:22 -0300

    refactor!  formatação de codigo

--- src/test/java/estudos/ecommerce/carrinho/adapter/out/persistence/CarrinhoRepositoryTest.java
+++ src/test/java/estudos/ecommerce/carrinho/adapter/out/persistence/CarrinhoRepositoryTest.java
@@ -2,21 +2,18 @@ package estudos.ecommerce.carrinho.adapter.out.persistence;
 
 import estudos.ecommerce.carrinho.domain.Carrinho;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class CarrinhoRepositoryTest {
-
 
     @Autowired
     private CarrinhoRepository carrinhoRepository;
